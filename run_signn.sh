f3c9f1e2285626f5f527136d9c596d0388ec6ef8
AriAlavi <arianalavi1@gmail.com>
2020-05-13 17:55:59 -0700

    Neural network piped into a python file

---
+++ run_signn.sh
@@ -0,0 +1,2 @@
+sudo bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_cpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/multi_hand_tracking_desktop_console_logger_zscores.pbtxt |  python3 signn.py
+
