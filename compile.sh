1486872756f4c4876ae8f9f79f71cdcb7198199a
AriAlavi <arianalavi1@gmail.com>
2020-07-04 12:33:15 -0700

    I don't know which bazel to compile with

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+<<<<<<< HEAD
 # Follow this guide to get any version of bazel: https://docs.bazel.build/versions/master/install-ubuntu.html
+=======
+# Follow this guide to get bazel 1.2.1: https://docs.bazel.build/versions/master/install-ubuntu.html
+>>>>>>> I don't know which bazel to compile with
 bazel build -c opt --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/multi_hand_tracking:multi_hand_tracking_cpu
