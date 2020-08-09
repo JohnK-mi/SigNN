b06445e0ed9e33c09c9ac56e854bc99b41850ae8
AriAlavi <arianalavi1@gmail.com>
2020-08-09 13:29:13 -0700

    Should work for mobile now

--- droidcam_run_gpu.sh
+++ droidcam_run_gpu.sh
@@ -1,1 +1,1 @@
-sudo GLOG_logtostderr=1 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libv4l/v4l2convert.so bazel-bin/mediapipe/examples/desktop/hand_tracking/hand_tracking_gpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/hand_tracking_mobile.pbtxt
+sudo GLOG_logtostderr=1 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libv4l/v4l2convert.so bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_gpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/multi_hand_tracking_mobile.pbtxt
--- run_gpu.sh
+++ run_gpu.sh
@@ -1,1 +1,1 @@
-sudo GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/hand_tracking/hand_tracking_gpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/hand_tracking_mobile.pbtxt
+sudo GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_gpu --calculator_graph_config_file=mediapipe/graphs/multi_hand_tracking/multi_hand_tracking_mobile.pbtxt
