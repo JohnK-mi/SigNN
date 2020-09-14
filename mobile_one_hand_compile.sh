b9bba3c77e64378a7b4b1abe516b82170b854618
AriAlavi <arianalavi1@gmail.com>
2020-09-14 16:55:25 -0700

    One hand signn now has its own section

--- mobile_one_hand_compile.sh
+++ mobile_one_hand_compile.sh
@@ -1,4 +1,4 @@
 export ANDROID_HOME=/home/python/Android/Sdk/
 export ANDROID_NDK_HOME=/home/python/Android/Sdk/ndk/21.3.6528147/
-bazel build -c opt --config=android_arm64 mediapipe/examples/android/src/java/com/google/mediapipe/apps/handtrackinggpu:handtrackinggpu
-adb install bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/handtrackinggpu/handtrackinggpu.apk
+bazel build -c opt --config=android_arm64 mediapipe/examples/android/src/java/com/google/mediapipe/apps/signnonehand:signnonehand
+adb install bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/signnonehand/signnonehand.apk
