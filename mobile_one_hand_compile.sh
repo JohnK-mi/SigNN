2f45e9fddf72e8f2548dfa790ccb20e8afde0ca8
AriAlavi <arianalavi1@gmail.com>
2020-09-14 15:45:10 -0700

    Basic single handed on mobile

---
+++ mobile_one_hand_compile.sh
@@ -0,0 +1,4 @@
+export ANDROID_HOME=/home/python/Android/Sdk/
+export ANDROID_NDK_HOME=/home/python/Android/Sdk/ndk/21.3.6528147/
+bazel build -c opt --config=android_arm64 mediapipe/examples/android/src/java/com/google/mediapipe/apps/handtrackinggpu:handtrackinggpu
+adb install bazel-bin/mediapipe/examples/android/src/java/com/google/mediapipe/apps/handtrackinggpu/handtrackinggpu.apk
