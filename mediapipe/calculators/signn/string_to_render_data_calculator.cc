4d1a38a87a569c058f722f7bf78715ba8f32ed34
AriAlavi <arianalavi1@gmail.com>
2020-09-14 17:27:54 -0700

    Increased size of text

--- mediapipe/calculators/signn/string_to_render_data_calculator.cc
+++ mediapipe/calculators/signn/string_to_render_data_calculator.cc
@@ -37,9 +37,9 @@ namespace mediapipe{
             auto* label_annotation = render_data->add_render_annotations();
             auto* text = label_annotation->mutable_text();
             text->set_display_text(given_string);
-            text->set_font_height(70);
+            text->set_font_height(80);
             text->set_normalized(false);
-            text->set_left(300);
+            text->set_left(350);
             text->set_baseline(450);
             label_annotation->mutable_color()->set_r(0);
             label_annotation->mutable_color()->set_g(0);
@@ -49,9 +49,9 @@ namespace mediapipe{
             auto* label_annotation_other = render_data->add_render_annotations();
             auto* text_other = label_annotation_other->mutable_text();
             text_other->set_display_text(given_string);
-            text_other->set_font_height(70);
+            text_other->set_font_height(80);
             text_other->set_normalized(false);
-            text_other->set_left(299);
+            text_other->set_left(349);
             text_other->set_baseline(451);
             label_annotation->mutable_color()->set_r(256);
             label_annotation->mutable_color()->set_g(256);
